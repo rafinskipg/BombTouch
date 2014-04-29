0419c5bf149a91c54a668bbd1d21c91d18903279
Rafael Pedrola <rafinskipg@gmail.com>
2014-04-29 09:45:45 +0200

    adding images

--- app/scripts/controllers/badges.js
+++ app/scripts/controllers/badges.js
@@ -7,7 +7,7 @@ define(['angular', 'app', 'maingame'], function(angular, BombTouchApp , GAME){
 
         $scope.badges = [
           {
-            name: 'thegame',
+            name: 'complete',
             alias: '',
             text: 'Complete the game'
           },
@@ -15,6 +15,36 @@ define(['angular', 'app', 'maingame'], function(angular, BombTouchApp , GAME){
             name: 'level1',
             alias: '',
             text: 'Kill all the level 1 monsters'
+          },
+          {
+            name: 'social',
+            alias: '',
+            text: 'Share your best score'
+          },
+          {
+            name: 'dogs25',
+            alias: '',
+            text: 'Get 25 doges'
+          },
+          {
+            name: 'dogs50',
+            alias: '',
+            text: 'Get 50 doges'
+          },
+          {
+            name: 'dogs100',
+            alias: '',
+            text: 'Get 100 doges'
+          },
+          {
+            name: 'dogs500',
+            alias: '',
+            text: 'Get 500 doges'
+          },
+          {
+            name: 'chaos',
+            alias: '',
+            text: 'Kill everything!'
           }
         ]
       }]);
